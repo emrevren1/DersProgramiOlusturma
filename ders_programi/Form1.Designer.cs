300e75ee56625537c51248c3d30df909f8777650
Emre Evren <141053926+emrevren1@users.noreply.github.com>
2023-07-31 20:54:38 +0300

    first commit

---
+++ ders_programi/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ders_programi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(290, 314);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "9-A",
+            "9-B",
+            "9-C",
+            "10-A",
+            "10-B",
+            "10-C",
+            "11-A",
+            "11-B",
+            "11-C",
+            "12-A",
+            "12-B",
+            "12-C"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(290, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SINIF SEÇİNİZ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 382);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ders_programi/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ders_programi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source =" + Application.StartupPath + "\\ders_programi.accdb");
+        OleDbDataAdapter da;
+        DataTable dt = new DataTable();
+        BindingSource bs = new BindingSource();
+
+        void alinacak_dersler_datagrid_temizle()
+        {
+            dataGridView1.DataSource = "";
+            bs.DataSource = "";
+            dt.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int a = 0;
+            a = comboBox1.SelectedIndex;
+            switch (a)
+            {
+                case 0:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,a_9 where dersler.ders_no=a_9.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 1:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,b_9 where dersler.ders_no=b_9.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 2:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,c_9 where dersler.ders_no=c_9.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 3:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,a_10 where dersler.ders_no=a_10.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 4:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,b_10 where dersler.ders_no=b_10.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 5:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,c_10 where dersler.ders_no=c_10.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 6:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,a_11 where dersler.ders_no=a_11.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 7:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,b_11 where dersler.ders_no=b_11.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 8:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,c_11 where dersler.ders_no=c_11.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 9:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,a_12 where dersler.ders_no=a_12.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 10:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,b_12 where dersler.ders_no=b_12.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+                case 11:
+                    alinacak_dersler_datagrid_temizle();
+                    da = new OleDbDataAdapter("select ders_adi,alacagi_ders_saati from dersler,c_12 where dersler.ders_no=c_12.alacagi_ders_no", con);
+                    da.Fill(dt);
+                    bs.DataSource = dt;
+                    dataGridView1.DataSource = bs;
+                    dataGridView1.Columns[0].Width = 140;
+                    break;
+            }
+        }
+    }
+}
